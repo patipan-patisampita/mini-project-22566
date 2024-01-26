831229aad4765313f4887aadde7091e8b155ec27
patipan-patisampita <patipan-patisampita@gmail.com>
2024-01-26 15:22:50 +0700

    update

--- signup.php
+++ signup.php
@@ -2,10 +2,17 @@
 
 <div class="container mt-4">
     <div class="row">
-        <div class="col-md-6">
-
+        <div class="col-md-4">
+            <div class="card shadow">
+                <div class="card-header p-0">
+                    <img src="assets/images/img.jpg" class="card-img-top" alt="img">
+                </div>
+                <div class="card-body">
+                    <p class="card-text text-end">Information Technology 2023.</p>
+                </div>
+            </div>
         </div>
-        <div class="col-md-6">
+        <div class="col-md-8">
 
         </div>
     </div>
