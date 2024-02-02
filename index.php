5566e05f106b0f48becb9df96ac1c0dd0b5cef22
patipan-patisampita <patipan-patisampita@gmail.com>
2024-02-02 14:39:05 +0700

    update

--- index.php
+++ index.php
@@ -1,5 +1,22 @@
+<?php session_start(); ?>
 <?php include("includes/header.php"); ?>
+<div class="pt-4">
+    <div class="container">
+        <div class="col-md-12">
+            <?php
+            if (isset($_SESSION["message"])) {
+            ?>
+                <div class="alert alert-warning alert-dismissible fade show" role="alert">
+                    <strong>Holy guacamole! </strong><?php echo $_SESSION["message"]; ?>
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>
 
-    <h1>Hello, world!</h1>
+            <?php
+                unset($_SESSION["message"]);
+            }
+            ?>
+        </div>
+    </div>
+</div>
 
 <?php include("includes/footer.php"); ?>
