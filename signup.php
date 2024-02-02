2911b5bb60197a38b1e01a1a6af9097145505f40
patipan-patisampita <patipan-patisampita@gmail.com>
2024-02-02 14:04:27 +0700

    update

--- signup.php
+++ signup.php
@@ -15,11 +15,11 @@
         </div>
         <div class="col-md-7">
            <?php 
-            if(isset($_SESSION  ["message"])) 
+            if(isset($_SESSION["message"])) 
             { 
             ?>
             <div class="alert alert-warning alert-dismissible fade show" role="alert">
-                <strong>Holy guacamole!</strong> You should check in on some of those fields below.
+                <strong>Holy guacamole! </strong><?php echo $_SESSION["message"]; ?>
                 <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
             </div>
 
