68de286c03d5224fb2b13015ac4553268d423772
patipan-patisampita <patipan-patisampita@gmail.com>
2024-02-02 14:01:34 +0700

    update

--- signup.php
+++ signup.php
@@ -1,3 +1,4 @@
+<?php session_start(); ?> 
 <?php include("includes/header.php"); ?>
 
 <div class="container mt-4">
@@ -13,6 +14,21 @@
             </div>
         </div>
         <div class="col-md-7">
+           <?php 
+            if(isset($_SESSION  ["message"])) 
+            { 
+            ?>
+            <div class="alert alert-warning alert-dismissible fade show" role="alert">
+                <strong>Holy guacamole!</strong> You should check in on some of those fields below.
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+
+            <?php
+            unset($_SESSION ["message"]);
+            }
+           ?>
+
+
             <div class="card shadow">
                 <div class="card-header">
                     <h5>ระบบลงทะเบียนผู้ใช้งาน</h5>
