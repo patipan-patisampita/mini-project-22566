3549a8276cc73f9fad460db3ec82dd89e19e35e7
patipan-patisampita <patipan-patisampita@gmail.com>
2024-02-02 13:34:48 +0700

    update

--- functions/authcode.php
+++ functions/authcode.php
@@ -8,8 +8,18 @@ if (isset($_POST["register_btn"])) {
     $phone = mysqli_real_escape_string($conn, $_POST["phone"]);
     $address = mysqli_real_escape_string($conn, $_POST["address"]);
 
-    if($password == $c_password){
+    if ($password == $c_password) {
         $insert_query = "INSER INTO users(name,email,password,phone,address)
         VALUES('$name','$email','$password',' $phone','$address')";
+        $insert_query_run = mysqli_query($conn, $insert_query);
+
+        if (mysqli_num_rows($insert_query_run) > 0) {
+            header('Location:../signin.php');
+        } 
+        else {
+            header('Location:../signup.php');
+        }
+    }else{
+        header('Location:../signup.php');
     }
 }
