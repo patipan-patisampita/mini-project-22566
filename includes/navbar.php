43749575cc35b7811adb2b1293d0c66939fd6619
patipan-patisampita <patipan-patisampita@gmail.com>
2024-02-02 15:03:00 +0700

    update

--- includes/navbar.php
+++ includes/navbar.php
@@ -11,24 +11,33 @@
                 </li>
 
                 <?php
-                if (isset($_SESSION['auth'])) 
-                {
+                if (isset($_SESSION['auth'])) {
                 ?>
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            ลิงค์เว็บอื่นๆ
+                            <?php print $_SESSION['auth_user']['name']; ?>
                         </a>
                         <ul class="dropdown-menu">
                             <li><a class="dropdown-item" href="https://www.trattc.ac.th/" target="_blank">วท.ตราด</a></li>
                             <li><a class="dropdown-item" href="https://www.vec.go.th/" target="_blank">อาชีวศึกษา</a></li>
+                            <li><a class="dropdown-item" href="https://www.moe.go.th/" target="_blank">กระทรวงศึกษาธิการ</a></li>
                             <li>
                                 <hr class="dropdown-divider">
                             </li>
-                            <li><a class="dropdown-item" href="https://www.moe.go.th/" target="_blank">กระทรวงศึกษาธิการ</a></li>
+                            <li><a class="dropdown-item" href="logout.php">ออกจากระบบ</a></li>
                         </ul>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" aria-disabled="true" href="#" target="_blank">ที่ตั้ง</a>
+                    </li>
+                <?php
+                } else {
+                ?>
+                    <li class="nav-item">
+                        <a href="signup.php" class="nav-link">ลงทะเบียน</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="signin.php" class="nav-link">เข้าสู่ระบบ</a>
                     </li>
                 <?php
                 }
