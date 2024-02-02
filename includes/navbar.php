1e2424aa49a45183390ba2663aa2875877f4e3e7
patipan-patisampita <patipan-patisampita@gmail.com>
2024-02-02 14:46:23 +0700

    update

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,32 +1,35 @@
 <nav class="navbar navbar-expand-lg navbar-dark shadow" style="background: #8427DC;">
     <div class="container">
-        <a class="navbar-brand" href="#">Navbar</a>
+        <a class="navbar-brand" href="index.php">Navbar</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    <a class="nav-link active" aria-current="page" href="index.php">หน้าแรก</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
+                    <a class="nav-link" href="signup.php">ลงทะเบียน</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="signin.php">เข้าสู่ระบบ</a>
                 </li>
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Dropdown
+                        ลิงค์เว็บอื่นๆ
                     </a>
                     <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="#">Action</a></li>
-                        <li><a class="dropdown-item" href="#">Another action</a></li>
+                        <li><a class="dropdown-item" href="https://www.trattc.ac.th/" target="_blank">วท.ตราด</a></li>
+                        <li><a class="dropdown-item" href="https://www.vec.go.th/" target="_blank
                         <li>
-                            <hr class="dropdown-divider">
+                            <hr class=" dropdown-divider">
                         </li>
-                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        <li><a class="dropdown-item" href="https://www.moe.go.th/" target="_blank">กระทรวงศึกษาธิการ</a></li>
                     </ul>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+                    <a class="nav-link" aria-disabled="true" href="#" target="_blank">ที่ตั้ง</a>
                 </li>
             </ul>
         </div>
