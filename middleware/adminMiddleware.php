6b38e924ee8a12a367516743ecf9da48a14055b1
patipan-patisampita <patipan-patisampita@gmail.com>
2024-02-09 15:21:11 +0700

    update

---
+++ middleware/adminMiddleware.php
@@ -0,0 +1,10 @@
+<?php
+if (isset($_SESSION['auth'])) {
+    if ($_SESSION['role_as'] != 1) {
+        $_SESSION['message'] = 'You are not authorzed to access this page';
+        header('Location: ../index.php');
+    } else {
+        $_SESSION['message'] = 'Login to continue';
+        header('Location: ../signin.php');
+    }
+}
